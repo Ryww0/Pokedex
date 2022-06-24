0d63111b7c85dd16f6197101bef1cb9878629203
Ryww0 <laurent.chaitas@gmail.com>
2022-06-24 15:21:29 +0200

    pokedex show name and image of the pokemon id user's typed

---
+++ app.js
@@ -0,0 +1,29 @@
+const btnNumbers = document.querySelectorAll(".case");
+const screenNumbers = document.querySelector(".screen-nmbr");
+const submit = document.querySelector(".btn-sub");
+const screenName = document.querySelector(".screen-name");
+const screenImg = document.querySelector(".screen");
+
+//clavier
+btnNumbers.forEach((element) => {
+  element.addEventListener("click", () => addNmbr(element.textContent));
+});
+
+function addNmbr(el) {
+  screenNumbers.innerHTML =
+    screenNumbers.textContent + btnNumbers[el].textContent;
+}
+
+//appel de l'api
+submit.addEventListener("click", () => {
+  let url = `https://pokebuildapi.fr/api/v1/pokemon/${screenNumbers.textContent}`;
+  requestApi(url);
+});
+function requestApi(url) {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      screenName.textContent = data.name;
+      screenImg.style.backgroundImage = `url("${data.image}")`;
+    });
+}
