e42b0667472c7d8a3cfa5832a8e390d10fbea40a
Ryww0 <laurent.chaitas@gmail.com>
2022-06-27 14:32:45 +0200

    ajout des types du pokemon choisit

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const screenNumbers = document.querySelector(".screen-nmbr");
 const submit = document.querySelector(".btn-sub");
 const screenName = document.querySelector(".screen-name");
 const screenImg = document.querySelector(".screen");
+const t1 = document.getElementById("t1");
+const t2 = document.getElementById("t2");
+
+//log variables
+// console.log(t1);
 
 //clavier
 btnNumbers.forEach((element) => {
@@ -23,8 +28,24 @@ function requestApi(url) {
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      screenName.textContent = data.name;
+      let apiTypes1 = data.apiTypes[0];
+      let apiTypes2 = data.apiTypes[1];
+      let test = data.apiTypes;
+
+      // console.log(test);
+
+      //nom du pokemon
+      screenName.textContent = `#${data.id} - ${data.name}`;
+      //image du pokemon
       screenImg.style.backgroundImage = `url("${data.image}")`;
+      //type 1
+      t1.style.backgroundImage = `url("${apiTypes1.image}")`;
+      //type 2 avec vérification
+      if (test.length >= 2) {
+        t2.style.backgroundImage = `url("${apiTypes2.image}")`;
+      } else {
+        t2.style.backgroundImage = ``;
+      }
     });
 
   screenNumbers.innerHTML = "";
