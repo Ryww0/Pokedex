c599b9b475e630ab267da3ce25fb17c10646e3a7
Ryww0 <laurent.chaitas@gmail.com>
2022-06-27 15:05:49 +0200

    description du pokemon

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const screenName = document.querySelector(".screen-name");
 const screenImg = document.querySelector(".screen");
 const t1 = document.getElementById("t1");
 const t2 = document.getElementById("t2");
+const description = document.querySelector(".screen-description");
 
 //log variables
 // console.log(t1);
@@ -30,9 +31,10 @@ function requestApi(url) {
     .then((data) => {
       let apiTypes1 = data.apiTypes[0];
       let apiTypes2 = data.apiTypes[1];
-      let test = data.apiTypes;
+      let typeLength = data.apiTypes;
+      let stats = data.stats;
 
-      // console.log(test);
+      console.log(stats);
 
       //nom du pokemon
       screenName.textContent = `#${data.id} - ${data.name}`;
@@ -41,10 +43,16 @@ function requestApi(url) {
       //type 1
       t1.style.backgroundImage = `url("${apiTypes1.image}")`;
       //type 2 avec vérification
-      if (test.length >= 2) {
+      if (typeLength.length >= 2) {
         t2.style.backgroundImage = `url("${apiTypes2.image}")`;
       } else {
         t2.style.backgroundImage = ``;
+      }
+      //stats
+      if (typeLength.length >= 2) {
+        description.textContent = `Les points de vie de ce pokémon s'élève à ${stats.HP}. ${data.name} possède une attaque de ${stats.attack} et une défense de ${stats.defense}. Sa vitesse est de ${stats.speed}. ${data.name} est un pokémon de type ${apiTypes1.name} et ${apiTypes2.name}`;
+      } else {
+        description.textContent = `Les points de vie de ce pokémon s'élève à ${stats.HP}. ${data.name} possède une attaque de ${stats.attack} et une défense de ${stats.defense}. Sa vitesse est de ${stats.speed}. ${data.name} est un pokémon de type ${apiTypes1.name}`;
       }
     });
 
